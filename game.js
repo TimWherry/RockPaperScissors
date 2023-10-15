b92acbb9966af0d74fda9e4826c2c358803ec211
TimWherry <TimWherry@gmail.com>
2023-10-15 14:37:13 -0600

    Add logic for round win lose or tie Player win is positive, loss is negative, tie is zero

--- game.js
+++ game.js
@@ -7,7 +7,53 @@
 //---------------------------------------
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    let roundResult = NaN
+    if(playerSelection == "rock")
+    {
+        if(computerSelection == "rock")
+        {
+            roundResult = 0
+        }
+        if(computerSelection == "paper")
+        {
+            roundResult = -1
+        }
+        if(computerSelection == "scissors")
+        {
+            roundResult = 1
+        }
+    }
+    if(playerSelection == "paper")
+    {
+        if(computerSelection == "rock")
+        {
+            roundResult = 1
+        }
+        if(computerSelection == "paper")
+        {
+            roundResult = 0
+        }
+        if(computerSelection == "scissors")
+        {
+            roundResult = -1
+        }
+    }
+    if(playerSelection == "scissors")
+    {
+        if(computerSelection == "rock")
+        {
+            roundResult = -1
+        }
+        if(computerSelection == "paper")
+        {
+            roundResult = 1
+        }
+        if(computerSelection == "scissors")
+        {
+            roundResult = 0
+        }
+    }
+    return roundResult
 }
 
 function getComputerChoice() {
@@ -23,7 +69,32 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function translatePlayerChoiceToGameOptions(playerSelection)
+{
+    if(playerSelection.toUpperCase() == "ROCK")
+    {
+        return "rock"
+    }
+    if(playerSelection.toUpperCase() == "PAPER")
+    {
+        return "paper"
+    }
+    if(playerSelection.toUpperCase() == "SCISSORS")
+    {
+        return "scissors"
+    }
+    return null
+}
+
+function playGame()
+{
+    const playerSelection = prompt("Rock, paper, or scissors?")
+    playerSelection = translatePlayerChoiceToGameOptions(playerSelection)
+
+    const computerSelection = getComputerChoice();
+
+    const playerWon = playRound(playerSelection, computerSelection);
+}
    
 //const playerSelection = "rock";
 //const computerSelection = getComputerChoice();
